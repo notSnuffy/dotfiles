4ac8f188171d4fd74fb328267db0b38e72e172aa
notSnuffy <96662342+notSnuffy@users.noreply.github.com>
2023-11-15 23:04:29 +0000

    Configured lsp + autocompletion

--- .config/nvim/lua/snuffy/lazy.lua
+++ .config/nvim/lua/snuffy/lazy.lua
@@ -11,4 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "snuffy.plugins" } })
+require("lazy").setup({ { import = "snuffy.plugins" }, { import = "snuffy.plugins.lsp" } })
---
+++ .config/nvim/lua/snuffy/plugins/nvim-cmp.lua
@@ -0,0 +1,43 @@
+return {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for file system paths
+        "L3MON4D3/LuaSnip", -- snippet engine
+        "saadparwaiz1/cmp_luasnip", -- for autocompletion
+        "rafamadriz/friendly-snippets", -- useful snippets
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        require("luasnip.loaders.from_vscode").lazy_load() -- load snippets from vscode
+
+        cmp.setup({
+            completion = {
+                completeopt = "menu,menuone,preview,noselect",
+            },
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+                ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            }),
+            sources = {
+                { name = "nvim_lsp" }, -- use lsp source
+                { name = "buffer" }, -- use buffer source
+                { name = "path" }, -- use path source
+                { name = "luasnip" }, -- use luasnip source
+            },
+        })
+    end,
+}
